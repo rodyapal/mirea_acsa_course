a9cec9210908d20b0a4bf76c8d1f86af23e4256a
Rodion Sviridov <sviridov.rod@yandex.ru>
2022-10-26 12:33:26 +0300

    practice 5 init

---
+++ practice5multilayerArchitecture/build.gradle.kts
@@ -0,0 +1,39 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val ktormVersion: String by project
+
+plugins {
+	application
+	kotlin("jvm") version "1.7.20"
+	id("io.ktor.plugin") version "2.1.2"
+	id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
+}
+
+group = "com.rodyapal"
+version = "0.0.1"
+application {
+	mainClass.set("com.rodyapal.ApplicationKt")
+
+	val isDevelopment: Boolean = project.ext.has("development")
+	applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+	implementation("io.ktor:ktor-server-host-common-jvm:$ktorVersion")
+	implementation("io.ktor:ktor-server-websockets-jvm:$ktorVersion")
+	implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+	implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
+	implementation("io.ktor:ktor-server-html-builder-jvm:$ktorVersion")
+	implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+	implementation("ch.qos.logback:logback-classic:$logbackVersion")
+	implementation("org.ktorm:ktorm-core:$ktormVersion")
+	testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
---
+++ practice5multilayerArchitecture/src/main/kotlin/com/rodyapal/Application.kt
@@ -0,0 +1,14 @@
+package com.rodyapal
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.rodyapal.plugins.*
+
+fun main() {
+	embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+		configureSockets()
+		configureSerialization()
+		configureTemplating()
+		configureRouting()
+	}.start(wait = true)
+}
